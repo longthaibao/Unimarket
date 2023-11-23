a197c096bfc2685decc0256f71276e9c582c0f64
TDung <91122031+dungnguyen73@users.noreply.github.com>
2023-11-23 16:26:43 +0700

    fix be admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,7 @@
 const ProductCategory = require("../../models/product-category.model");
-
+const Product         = require("../../models/product.model");
+const Account         = require("../../models/account.model");
+const User            = require("../../models/user.model");
 // [GET] /admin/dashboard
 module.exports.dashboard = async (req, res) => {
     const statistic = {
@@ -38,7 +40,55 @@ module.exports.dashboard = async (req, res) => {
       status: "inactive",
       deleted: false
     });
-  
+    
+    statistic.product.total = await Product.count({
+      deleted:false
+    });
+
+    statistic.product.active = await Product.count({
+      status: "active",
+      deleted: false
+    });
+
+    statistic.product.inactive = await Product.count({
+      status: "inactive",
+      deleted:false
+    });
+
+    statistic.account.total = await Account.count({
+      deleted: false
+    });
+
+    statistic.account.active = await Account.count({
+      status: "active",
+      deleted: false
+    });
+
+    statistic.account.inactive = await Account.count({
+      status: "inactive",
+      deleted:false
+    });
+
+    statistic.user.total = await User.count({
+      deleted: false
+    });
+
+    statistic.user.active = await User.count({
+      status: "active",
+      deleted: false
+    });
+
+    statistic.user.inactive = await User.count({
+      status: "inactive",
+      deleted:false
+    });
+
+
+
+
+
+
+
     res.render("admin/pages/dashboard/index", {
         pageTitle: "Trang tổng quan",
         statistic: statistic
