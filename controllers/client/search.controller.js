6785c1b26e3c0d35711232765b3831623fd81059
Duke0503 <minhducgl0503@gmail.com>
2023-11-25 17:08:31 +0700

    Update BE Filter

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,27 +1,40 @@
 const Product = require("../../models/product.model");
 
+const filterPriceHelper = require("../../helpers/filterPrice");
 const productsHelper = require("../../helpers/products");
 
 // [GET] /search
 module.exports.index = async (req, res) => {
-  const keyword = req.query.keyword;
-
+  let match = {};
   let newProducts = [];
 
-  if(keyword) {
-    const regex = new RegExp(keyword, "i");
-    const products = await Product.find({
-      title: regex,
-      deleted: false,
-      status: "active"
-    });
-
-    newProducts = productsHelper.priceNewProducts(products);
+  if (req.query.keyword) {
+    match.$or = [
+      { title: new RegExp(req.query.keyword, "i") },
+      { school: new RegExp(req.query.keyword, "i") },
+    ];
   }
 
+  if (req.query.school) {
+    match.school = new RegExp(req.query.school, "i");
+  }
+
+  if (req.query.facet) {
+    match.product_category_id = req.query.facet;
+  }
+  match.deleted = false;
+  match.status = "active";
+
+  const products = await Product.find(match);
+
+  if (req.query.minPrice || req.query.maxPrice) {
+    let filterPrice = filterPriceHelper(products, req.query.minPrice, req.query.maxPrice);
+    newProducts = productsHelper.priceNewProducts(filterPrice)
+  } else newProducts = productsHelper.priceNewProducts(products); 
+  
   res.render("client/pages/search/index", {
     pageTitle: "Kết quả tìm kiếm",
-    keyword: keyword,
+    keyword: req.query.keyword,
     products: newProducts
   });
 }
